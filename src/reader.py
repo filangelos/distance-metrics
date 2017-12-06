21d1c504f681cca7b93259079756cfc2642eacfe
filangel <filos.angel@gmail.com>
2017-12-06 15:19:44 +0000

    reader: init csv

--- src/reader.py
+++ src/reader.py
@@ -1,16 +1,14 @@
 # scientific computing library
 import numpy as np
-# `.mat` to `Python`-compatible data converter
-import scipy.io
 
 
-def fetch_data(fname='face', ratio=0.8):
+def fetch_data(fname='wine.data', ratio=0.8):
     """Bootstrapping helper function for fetching data.
 
     Parameters
     ----------
     fname: str
-        Name of the `.mat` input file
+        Name of the `.csv` input file
     ratio: float
         Split ratio of dataset
 
@@ -25,32 +23,7 @@ def fetch_data(fname='face', ratio=0.8):
             - y: labels
     """
 
-    # load `.mat` file
-    data = scipy.io.loadmat('data/%s.mat' % fname)
+    # load `.csv` file
+    data = np.loadtxt('data/%s.csv' % fname, delimiter=' ')
 
-    # Images
-    # N: number of images
-    # D: number of pixels
-    _X = data['X']  # shape: [D x N]
-    _y = data['l']  # shape: [1 x N]
-
-    assert(_X.shape[1] == _y.shape[1])
-    # Number of images
-    D, N = _X.shape
-
-    # Fix the random seed
-    np.random.seed(13)
-
-    # Shuffled indeces
-    _mask = np.arange(N)
-    np.random.shuffle(_mask)
-
-    # Randomised data
-    X = _X[:, _mask]
-    y = _y[:, _mask]
-
-    # Ratition dataset to train and test sets
-    X_train, X_test = X[:, :int(N * ratio)], X[:, int(N * ratio):]
-    y_train, y_test = y[:, :int(N * ratio)], y[:, int(N * ratio):]
-
-    return {'train': (X_train, y_train), 'test': (X_test, y_test)}
+    return data
